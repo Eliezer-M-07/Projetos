604d9e3b167ad9f4c2228dd95a72dc712752189e
Eliézer.M <94458792+DarkBounty@users.noreply.github.com>
2022-08-13 14:05:52 -0300

    Update GeradorDeSenhas.py

--- GeradorDeSenhas.py
+++ GeradorDeSenhas.py
@@ -6,23 +6,33 @@ from random import shuffle
 caracteres = [1,2,3,4,5,6,7,8,9,0,'!','@','/','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','*',]
 
 shuffle(caracteres)
+while True:
+    tamanhoSenha = input('Deseja senha media ou longa? (digite "media" ou "longa") ')
 
-tamanhoSenha = input('Deseja senha media ou longa? (digite "media" ou "longa") ')
+    if tamanhoSenha == 'media':
+        senha = caracteres[0:9]
+        print('\nSUA SENHA:')
+        for i in senha:
+            print(i,end='')
+        break
 
-if tamanhoSenha == 'media':
-    senha = caracteres[0:9]
-    print('\nSUA SENHA:')
-    for i in senha:
-        print(i,end='')
+    elif tamanhoSenha == 'longa':
+        senha = caracteres[0:14]
+        print('\nSUA SENHA:')
+        for i in senha:
+            print(i,end='')
+            
+        break
 
-elif tamanhoSenha == 'longa':
-    senha = caracteres[0:14]
-    print('\nSUA SENHA:')
-    for i in senha:
-        print(i,end='')
+    else:
+        print('Informe um dos tamanhos de senha.')
 
-    
-    
+        
+        
+
+
+
+
 
 
 
